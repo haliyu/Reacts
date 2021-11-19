fb3bc12c965f057e0d0052730535f74b84827648
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-19 23:29:13 +0100

    second

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,35 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchtour = async ()=>{
+    setLoading(true)
+      try {
+          const response = await fetch(url);
+          const tours = response.json();
+          setTours(tours);
+          setLoading(false)
+      } catch (error) {
+          console.log(error);
+          setLoading(false)
+      }
+  };
+  
+  useEffect(()=>{
+    fetchtour();
+  },[]);
+
+  if (loading) {
+    return(
+      <Loading />
+    )
+  }
+
+
+  return <>
+      <Tours tours={tours} />
+  </>
 }
 
 export default App
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, name, price}) => {
+  return(
+      <article className='single-tour' key={id}>
+            <img src={image} />
+            <footer>
+                <div className='tour-info'>
+                  <h4>{name}</h4>
+                  <p>{price}</p>
+                </div>
+                <p>{info}</p>
+                <button className='delete-btn'>Not Interested</button>
+            </footer>
+      </article>
+  );
 };
 
 export default Tour;
