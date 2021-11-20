7db87b6af4d6be1bd69f84b043b03cbab6f5d703
haliyu <61690728+haliyu@users.noreply.github.com>
2021-11-20 01:07:41 +0100

    edit

--- 02-tours/setup/src/Loading.js
+++ 02-tours/setup/src/Loading.js
@@ -15,7 +15,7 @@ function Loading() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>loading...</h3>
+        <h3 style={{color: '#F0C38E'}}>loading...</h3>
       </header>
     </div>
   );
