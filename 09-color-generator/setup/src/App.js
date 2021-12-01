ec576858f5af1ec64a4ce3b6aa5330d69bde63db
haliyu <61690728+haliyu@users.noreply.github.com>
2021-12-01 19:52:34 +0100

    done

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,44 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [list, setList] = useState(new Values('#D47AE8').all(10));
+  const [error, setError] = useState(false);
+
+  const handle = (e)=>{
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      console.log(error)
+      setError(true)
+    }
+  }
+
+  return (<main>
+    <form className='container' onSubmit={handle}>
+      <h3>Color Generator</h3>
+      <input 
+      type='text'
+      onChange={(e)=> setColor(e.target.value)}
+      placeholder='#D47AE8'
+      value={color}
+
+      />
+      <button className='btn' type='submit'>Submit</button>
+    </form>
+    <div className='colors'>{
+      list.map((color,index)=>{
+        return<SingleColor 
+        key={index}
+        {...color}
+        index={index}
+        hexColor={color.hex}
+        />
+      })}</div>
+  </main>)
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({index, hexColor,rgb,weight}) => {
+  const [alart, setAlart] = useState(false);
+  const bcg = rgb.join(',')
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  console.log(hexValue)
+  
+
+
+  return (<div className='color'
+  className={`color ${index > 10 && 'color-light'}`}
+  style={{backgroundColor: `rgb(${bcg})`}}
+  onClick={()=>{ setAlart(true)
+    Navigator.clipboard.writeText(hexValue)
+  }}>
+    <p className='percent-value'>{weight}%</p>
+    <p className='color-value'>{hexValue}</p>
+    {alart && <p className='alart'>copied to clipboard</p>}
+  </div>)
 }
 
 export default SingleColor
